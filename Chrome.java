d242e8782eb2d10aa437cb33b92683b249931b0d
ShunPriya <csshunmugapriya@gmail.com>
2025-07-25 11:53:32 +0530

    Add files via upload
    This is Week 1 Day 1 Assignment. 
    Creating different Data Types

---
+++ Chrome.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		//float
+		float version = 91.0f;
+		System.out.println("Float is " + version);
+		//string
+		String developer = "Google";
+		System.out.println("String is "+ developer);
+		//boolean
+		boolean isBeta = false;
+		System.out.println("boolean is "+ isBeta);
+		//int
+		int releaseYear = 2008;
+		System.out.println("integer is "+ releaseYear);
+		//char
+		char shortcutKey = 'C';
+		System.out.println("characrter is "+ shortcutKey);
+	}
+
+}
